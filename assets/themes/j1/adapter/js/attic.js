476b94ad78019ee61a402f1f5810e6dfb7c4624b
chevrm <chevrm@gmail.com>
2023-09-11 12:35:17 -0400

    Updates

--- assets/themes/j1/adapter/js/attic.js
+++ assets/themes/j1/adapter/js/attic.js
@@ -17,7 +17,7 @@
  # Backstretch is licensed under the MIT License.
  # For details, see https://github.com/jquery-backstretch/jquery-backstretch
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2023-09-03 17:37:00 -0400
+ #  Adapter generated: 2023-09-11 12:35:03 -0400
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -45,7 +45,7 @@ j1.adapter.attic = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.attic',
-        generated:   '2023-09-03 17:37:00 -0400'
+        generated:   '2023-09-11 12:35:03 -0400'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
