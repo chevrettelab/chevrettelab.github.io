816a413e4fa8fe7d8ff68fc5f1ea5d860dae93b8
chevrm <chevrm@gmail.com>
2025-06-26 08:42:08 -0400

    Updates

--- assets/themes/j1/adapter/js/asciidoctor.js
+++ assets/themes/j1/adapter/js/asciidoctor.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2025-06-26 08:33:43 -0400
+ #  Adapter generated: 2025-06-26 08:41:52 -0400
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -44,7 +44,7 @@ j1.adapter.asciidoctor = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.rtable',
-        generated:   '2025-06-26 08:33:43 -0400'
+        generated:   '2025-06-26 08:41:52 -0400'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
--- assets/themes/j1/adapter/js/carousel.js
+++ assets/themes/j1/adapter/js/carousel.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # See: https://github.com/jekyll-one-org/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2025-06-26 08:33:43 -0400
+ #  Adapter generated: 2025-06-26 08:41:52 -0400
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -45,7 +45,7 @@ j1.adapter.carousel = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.carousel',
-        generated:   '2025-06-26 08:33:43 -0400'
+        generated:   '2025-06-26 08:41:52 -0400'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
